b9fd334154002800a24b35b785c3cf30153089b9
billh <billh@10.1.10.74>
2018-12-21 15:24:32 -0500

    added templates

---
+++ src/main/java/com/clearavenue/helloworld/controller/CMSController.java
@@ -0,0 +1,14 @@
+package com.clearavenue.helloworld.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CMSController {
+	
+	@GetMapping("/cms")
+	public String home(final ModelMap model) {
+		return "cms";
+	}
+}
